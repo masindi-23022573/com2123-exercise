ecc1c0f20465a27b3d697fa5bd5c9173cf308ae4
masindi-23022573 <166657733+masindi-23022573@users.noreply.github.com>
2024-04-18 20:44:12 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+//array of requirements
+string requirements[]={
+    "metal capacity"
+    "age group"
+    "body type"
+    "personality"
+    "maturity"
+    "height"
+    "skin tone"
+    "beauty"
+};
+}
+
+
+
+
+
+s  if (mental capacity is normal){
+    cout<<"yes"
+    }else (not normal){
+    cout<<"no"
+}
+//prompt user for input
+   cout<<"enter your age "<<endl;
+   int age group
+   cin >> age;
+
+   if (age >=18){
+   cout<<"yes"<<endl;
+   }else if (age<21)
+   cout<<"consider"<<endl;
+   else(age <25)
+    cout<<"too old for me"<<endl;
+   }
+ if(is a slender){
+ cout<<"yes"<<endl;
+ }else if("is a chubby"){
+ cout<<"no"<<endl;
+
+ if(personality is right){
+ cout<<" yes"<<endl;
+ else if ("personality is not right")
+ cout<<"no"<<endl;
+
+ if(is a yellow bone ){
+ cout<<"yes"<<endl;
+ }else if(is not a yellow bone){
+
+ cout<<"no"<<endl;
+
+ if(has money){
+ cout<<"yes"<<endl;
+}else(no money){
+ cout<<"no"<<endl;
+
+return  0;
+
+   }
+ }
+
+
